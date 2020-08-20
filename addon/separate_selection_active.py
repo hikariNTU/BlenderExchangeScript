29699ce8f54ed2a5870d4a46ef9691c3473dcc3b
Chung, Lian <hikari1286@iis.sinica.edu.tw>
2020-08-20 10:53:48 +0800

    SSA preference disable invoke behavior

--- addon/separate_selection_active.py
+++ addon/separate_selection_active.py
@@ -2,17 +2,37 @@
 # [python - code for "separate and select it in object mode" - Blender Stack Exchange]
 # (https://blender.stackexchange.com/questions/191394/code-for-separate-and-select-it-in-object-mode/191412#191412)
 import bpy
+from bpy.types import AddonPreferences
+from bpy.props import BoolProperty
 
 bl_info = {
     "name": "Separate Selection Active",
     "description": "Separate the mesh and select them immediately.",
     "author": "hikariTW",
-    "version": (0, 2),
+    "version": (0, 4),
     "blender": (2, 80, 0),
     "location": "View3D > Mesh",
     "support": "TESTING",
     "category": "Mesh",
 }
+
+
+class SSAAddonPreferences(AddonPreferences):
+    # this must match the add-on name, use '__package__'
+    # when defining this in a submodule of a python package.
+    bl_idname = __name__
+
+    use_invoke: BoolProperty(
+        name="Use popup invoke menu",
+        description="Show popup when calling operator",
+        default=False,
+    )
+
+    def draw(self, context):
+        layout = self.layout
+        layout.label(text="Setting for operator behavior.")
+        layout.prop(self, "use_invoke")
+
 
 _separate_method_enum = bpy.props.EnumProperty(
     items={
@@ -39,8 +59,12 @@ class SeparateSelectionActive(bpy.types.Operator):
         return context.object is not None and context.mode == 'EDIT_MESH'
 
     def invoke(self, context, event):
+        addon_prefs = context.preferences.addons[__name__].preferences
         # Prompt to ask a method to separate
-        return context.window_manager.invoke_props_dialog(self)
+        if addon_prefs.get('use_invoke', True):
+            return context.window_manager.invoke_props_dialog(self)
+        else:
+            return self.execute(context)
 
     def execute(self, context):
         org_obj_list = {o.name for o in context.selected_objects}
@@ -61,15 +85,25 @@ class SeparateSelectionActive(bpy.types.Operator):
 
 # A menu inject into View3D > Edit > Mesh tab
 def _menu_func(self, context):
-    self.layout.operator(SeparateSelectionActive.bl_idname)
+    addon_prefs = context.preferences.addons[__name__].preferences
+    if addon_prefs.get('use_invoke',True):
+        self.layout.operator(SeparateSelectionActive.bl_idname)
+    else:
+        self.layout.operator_menu_enum(
+            SeparateSelectionActive.bl_idname,
+            property='separate_method',
+            text="Separate, Selection Active"
+        )
 
 
 def register():
     bpy.utils.register_class(SeparateSelectionActive)
+    bpy.utils.register_class(SSAAddonPreferences)
     bpy.types.VIEW3D_MT_edit_mesh.append(_menu_func)
 
 
 def unregister():
+    bpy.utils.unregister_class(SSAAddonPreferences)
     bpy.utils.unregister_class(SeparateSelectionActive)
     bpy.types.VIEW3D_MT_edit_mesh.remove(_menu_func)
 
